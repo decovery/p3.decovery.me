7c496d92c9a22842e0eac2bbaff735b03a4cf52d
decovery <aiste@akmila.lt>
2013-12-07 02:57:49 +0200

    Project files added

---
+++ js/pizza-builder.js
@@ -0,0 +1,55 @@
+//Listeners
+$('input, select').change(calculate);
+
+$('input:radio[name=pad]').click(function() {
+	
+	var new_image = '<img src="images/pizza-small.png" width="280px;" alt="" />';
+	
+	$('#canvas').prepend(new_image);
+
+});
+
+//Functions
+function calculate() {
+
+	var base = parseFloat($('input:radio[name=pad]:checked').val());
+	var quantity = $('#quantity').val();
+	var toppings = $('input[name=addition]:checked');
+	var total = 0;
+	var price = 0;
+	var amount = 0;
+	
+	// Calculate chosen toppings
+	toppings.each(function() {
+	
+		price += parseFloat($(this).val());
+	
+	});
+	
+	//Add comments
+	if (price == 0) {
+		$('#info').html('You have chosen pizza Margherita!');
+	}
+	else if ( price > 0) {
+		$('#info').html('Great choise!');
+	}
+	
+	// Add toppings to the chosen base
+	amount = amount + price + base;
+	
+	// Multiply the price of one pizza to the quantity chosen
+	total = (amount * quantity).toFixed(2);
+		
+	// If user starts choosing toppings without having chosen the base
+	if(isNaN(total)) {
+		$('#alert').html('Oops! You should choose your pizza size first.');
+		$('#info').html('');
+		total = 0;
+	}
+	else {
+		$('#alert').html('');
+	}
+	
+	// Display total
+		$('#summary').html(total);
+}
